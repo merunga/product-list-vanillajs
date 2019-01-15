ba3c3dbf2fe3b0be4db22b2509d87cadbef370ce
merunga <merunga@gmail.com>
2019-01-15 18:10:33 -0500

    acciones producto listeners

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,12 +1,16 @@
+import store from '../store.js';
+import { productoIncStock, productoDecStock, productoEliminar } from '../controller.js';
+import { renderUI } from '../view-controller.js';
+
 const ProductRow = ({ producto }) => (`
   <tr data-id="${producto.id}" class="product ${producto.stock === 0 ? 'no-stock' : ''}">
     <td>${producto.name}</td>
     <td class="price">${producto.price.toFixed(2)}</td>
     <td class="stock">${producto.stock}</td>
     <td>
-      <button title="inc stock">+</button>
-      <button title="dec stock">-</button>
-      <button title="eliminar">x</button>
+      <button class="btn-inc" title="inc stock">+</button>
+      <button class="btn-dec" title="dec stock">-</button>
+      <button class="btn-eliminar" title="eliminar">x</button>
     </td>
   </tr>
   `);
@@ -31,5 +35,27 @@ export default ({ productos }) => {
   const divElem = document.createElement('div');
   divElem.setAttribute('id', 'product-list');
   divElem.innerHTML = htmlContent;
+  productos.forEach((p) => {
+    const btnInc = divElem.querySelector(`[data-id="${p.id}"] .btn-inc`);
+    btnInc.addEventListener('click', () => {
+      const result = productoIncStock({ productos, productoId: p.id });
+      store.set('productos', result);
+      renderUI();
+    });
+
+    const btnDec = divElem.querySelector(`[data-id="${p.id}"] .btn-dec`);
+    btnDec.addEventListener('click', () => {
+      const result = productoDecStock({ productos, productoId: p.id });
+      store.set('productos', result);
+      renderUI();
+    });
+
+    const btnEliminar = divElem.querySelector(`[data-id="${p.id}"] .btn-eliminar`);
+    btnEliminar.addEventListener('click', () => {
+      const result = productoEliminar({ productos, productoId: p.id });
+      store.set('productos', result);
+      renderUI();
+    });
+  });
   return divElem;
 };
