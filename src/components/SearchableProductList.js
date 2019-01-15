dd431721824f452972bd3a61b20095bc766ac5ae
merunga <merunga@gmail.com>
2019-01-15 16:41:10 -0500

    store fijo

--- src/components/SearchableProductList.js
+++ src/components/SearchableProductList.js
@@ -11,15 +11,7 @@ const ProductRow = ({ product }) => (`
   </tr>
   `);
 
-export default () => {
-  const products = [
-    { id: 1, price: "$49.99", stock: 3, name: "Football"},
-    { id: 2, price: "$9.99", stock: 3, name: "Baseball"},
-    { id: 3, price: "$29.99", stock: 0, name: "Basketball"},
-    { id: 4, price: "$99.99", stock: 2, name: "iPod Touch"},
-    { id: 5, price: "$399.99", stock: 0, name: "iPhone 5"},
-    { id: 6, price: "$199.99", stock: 1, name: "Nexus 7"},
-  ];
+export default ({ products }) => {
   const htmlContent = `
 <h3>Productos</h3>
 <div id="product-search-form">
--- src/view-controller.js
+++ src/view-controller.js
@@ -1,10 +1,13 @@
 import NewProductForm from './components/NewProductForm.js';
 import SearchableProductList from './components/SearchableProductList.js';
+import store from './store.js';
 
 export const renderComponents = () => {
+  const { products } = store.get();
+
   const root = document.getElementById('root');
   const form = NewProductForm();
-  const list = SearchableProductList();
+  const list = SearchableProductList({ products });
   root.appendChild(form);
   root.appendChild(list);
 }
