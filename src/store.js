ed8dfe3935001421423fc8f6fff726b91eb5a81a
merunga <merunga@gmail.com>
2019-01-15 18:15:21 -0500

    local storage

--- src/store.js
+++ src/store.js
@@ -1,25 +1,4 @@
-const data = {
-  productos: [
-    {
-      id: 1, price: 49.99, stock: 3, name: 'Football',
-    },
-    {
-      id: 2, price: 9.99, stock: 3, name: 'Baseball',
-    },
-    {
-      id: 3, price: 29.99, stock: 0, name: 'Basketball',
-    },
-    {
-      id: 4, price: 99.99, stock: 2, name: 'iPod Touch',
-    },
-    {
-      id: 5, price: 399.99, stock: 0, name: 'iPhone 5',
-    },
-    {
-      id: 6, price: 199.99, stock: 1, name: 'Nexus 7',
-    },
-  ],
-};
+const data = JSON.parse(localStorage.getItem('data') || '{ "productos": [] }');
 
 export default {
   get(field) {
@@ -27,5 +6,6 @@ export default {
   },
   set(field, value) {
     data[field] = value;
+    localStorage.setItem('data', JSON.stringify(data));
   },
 };
