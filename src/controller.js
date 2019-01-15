c05ea00972e5ec02a53eaff1f0d2bdbe57683627
merunga <merunga@gmail.com>
2019-01-15 16:48:31 -0500

    eslint conf

---
+++ src/controller.js
@@ -0,0 +1,19 @@
+export const productoCrear = () => {
+
+};
+
+export const productoIncStock = () => {
+  
+};
+
+export const productoDeccStock = () => {
+  
+};
+
+export const productoEliminar = () => {
+  
+};
+
+export const productoFiltrar = () => {
+
+};
